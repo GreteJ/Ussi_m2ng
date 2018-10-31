499ffc607e99a442623044d3b31b15ecd460c800
RobertRBO <43957361+RobertRBO@users.noreply.github.com>
2018-10-31 21:41:15 +0200

    Update Ussi_m2ng.py

--- Ussi_m2ng.py
+++ Ussi_m2ng.py
@@ -13,3 +13,14 @@ class Uss():
         self.position = [100,50]
         self.body = [[100,50],[90,50],[80,50]]
         self.direction = "RIGHT"
+
+#vajutades neid klahve muudab uss suunda        
+    def muudaSuunda(self, suund):
+        if suund=="RIGHT" and not self.direction=="LEFT":
+            self.direction = "RIGHT"
+        if suund=="LEFT" and not self.direction=="RIGHT":
+            self.direction = "LEFT"
+        if suund=="UP" and not self.direction=="DOWN":
+            self.direction = "UP"
+        if suund=="DOWN" and not self.direction=="UP":
+            self.direction = "DOWN"        
