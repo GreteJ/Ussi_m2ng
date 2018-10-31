7c1f24579e04e938e64eed5fe4c76eb2ceb00266
GreteJ <GreteJaeski@github.com>
2018-10-31 21:32:24 +0200

    Update Ussi_m2ng.py

---
+++ Ussi_m2ng.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+import random
+import time
+
+pg = pygame
+
+pg.image.load("taust.png")
+
+#uss, selle asukoht, liikumine, pikenemine, suremine
+class Uss():
+    def __init__(self):
+        self.position = [100,50]
+        self.body = [[100,50],[90,50],[80,50]]
+        self.direction = "RIGHT"
